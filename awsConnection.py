f1a4c65576cbc4005e7d3032262bc9ca564cec5b
samuelsantos22 <samuel.espiritosanto@outlook.com>
2019-11-17 14:35:13 -0300

    Update awsConnection.py

--- awsConnection.py
+++ awsConnection.py
@@ -3,10 +3,13 @@ import boto3 as b3
 class awsConnection():
 	def __init__(self):
 		self.awsRekognitionClient = 0
-		self.key_id = "AKIA2QDMHXIAOL7KQ4ZO"
-		self.secret_key = "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"
+		self.key_id = "*******************"
+		self.secret_key = "*******************"
 		self.aws_region = "us-west-2"
-
+		#Complete ******** with your credentials
+		
 	def getRekognitionClient(self):
 		self.awsRekognitionClient = b3.client('rekognition', aws_access_key_id = self.key_id, aws_secret_access_key = self.secret_key, region_name = self.aws_region)
 		print("[+]Connected to AWS Rekognition")
+
+	
